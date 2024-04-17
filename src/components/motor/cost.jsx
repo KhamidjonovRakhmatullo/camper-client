239cb2337b4ec1050678d99094b34ef3ab13e86d
Bekzod Abdulkhaev <abdulkhaev96@gmail.com>
2024-04-17 09:31:48 +0900

    initial

--- src/components/motor/cost.jsx
+++ src/components/motor/cost.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Border, CompareBox, CostText, CostTitle, SearchButton } from "./motorStyled";
+import {
+  Border,
+  CompareBox,
+  CostText,
+  CostTitle,
+  SearchButton,
+} from "./motorStyled";
 import { DispJustAlgn } from "../../styles/styled";
 import grommet2 from "../assets/grommet.svg";
 import compare22 from "../assets/compare2.png";
 
 const CostComponent = () => {
   return (
-    <DispJustAlgn
-      $column
-      $alignStart
-      style={{ border: "0px solid red", gap: "50px" }}
-    >
+    <DispJustAlgn $column $alignStart style={{ gap: "50px" }}>
       <DispJustAlgn $column $alignStart>
         <CostTitle>Cost of car</CostTitle>
-        <Border ></Border>
+        <Border></Border>
         <DispJustAlgn style={{ padding: "20px 0px 50px 0px", gap: "25px" }}>
           <DispJustAlgn $column $alignStart>
             <CostText>from</CostText>
@@ -33,7 +35,9 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart>
-        <DispJustAlgn style={{ justifyContent: "space-between", width: "100%"}}>
+        <DispJustAlgn
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
           <CostTitle>Brand</CostTitle>
           <img src={grommet2} alt="icon" />
         </DispJustAlgn>
@@ -52,7 +56,9 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart>
-        <DispJustAlgn style={{ justifyContent: "space-between", width: "100%"}}>
+        <DispJustAlgn
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
           <CostTitle>Company</CostTitle>
           <img src={grommet2} alt="icon" />
         </DispJustAlgn>
@@ -68,7 +74,9 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart>
-        <DispJustAlgn style={{ justifyContent: "space-between", width: "100%" }}>
+        <DispJustAlgn
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
           <CostTitle>License type</CostTitle>
           <img src={grommet2} alt="icon" />
         </DispJustAlgn>
@@ -87,7 +95,9 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart>
-        <DispJustAlgn style={{ justifyContent: "space-between", width: "100%" }}>
+        <DispJustAlgn
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
           <CostTitle>Number of travelers</CostTitle>
           <img src={grommet2} alt="icon" />
         </DispJustAlgn>
@@ -106,7 +116,9 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart>
-        <DispJustAlgn style={{ justifyContent: "space-between", width: "100%" }}>
+        <DispJustAlgn
+          style={{ justifyContent: "space-between", width: "100%" }}
+        >
           <CostTitle>Location</CostTitle>
           <img src={grommet2} alt="icon" />
         </DispJustAlgn>
@@ -130,7 +142,7 @@ const CostComponent = () => {
       </DispJustAlgn>
 
       <DispJustAlgn $column $alignStart $gap>
-        <p style={{color: "#006DAB"}}>Compare</p>
+        <p style={{ color: "#006DAB" }}>Compare</p>
         <DispJustAlgn $gap $justifyStart>
           <CompareBox>
             <img src={compare22} alt="img" />
--- src/components/motor/motor.jsx
+++ src/components/motor/motor.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import { DispJustAlgn } from '../../styles/styled'
-import { CostText, CostTitle, MotorMainBg, Price } from './motorStyled'
-import CostComponent from './cost'
-import DetailBoxesNavComponent from './detailBoxesNav'
-import CarDetailBoxesComponent from './carDetailBoxes'
+import React from "react";
+import { DispJustAlgn } from "../../styles/styled";
+import { CostText, CostTitle, MotorMainBg, Price } from "./motorStyled";
+import CostComponent from "./cost";
+import DetailBoxesNavComponent from "./detailBoxesNav";
+import CarDetailBoxesComponent from "./carDetailBoxes";
 
 const MotorComponent = () => {
   return (
     <DispJustAlgn $column>
-       <MotorMainBg>
-        <CostText $fontSize $colorWhite>Home/Motor</CostText>
-        <CostTitle $fontSize27 $fontWeight700 $paddingTop style={{color: "#fff"}} >Our Ranges</CostTitle>
-        <Price $fontSize80 $colorWhiteH1>Motors</Price>
-       </MotorMainBg>
-       <DispJustAlgn $justifyStart $alignStart  style={{backgroundColor: "#FAFAFA", padding: "60px 70px 0px 70px", width: "100%",}}>
-        <div className='LeftSideBar' style={{flex: "1",}}>
-            <CostComponent></CostComponent>
+      <MotorMainBg>
+        <CostText $fontSize $colorWhite>
+          Home/Motor
+        </CostText>
+        <CostTitle
+          $fontSize27
+          $fontWeight700
+          $paddingTop
+          style={{ color: "#fff" }}
+        >
+          Our Ranges
+        </CostTitle>
+        <Price $fontSize80 $colorWhiteH1>
+          Motors
+        </Price>
+      </MotorMainBg>
+      <DispJustAlgn
+        $justifyStart
+        $alignStart
+        style={{
+          backgroundColor: "#FAFAFA",
+          padding: "0 20%",
+          width: "100%",
+        }}
+      >
+        <div className="LeftSideBar" style={{ flex: "1" }}>
+          <CostComponent></CostComponent>
         </div>
 
-        <div style={{flex: "10",}}>
-            <DetailBoxesNavComponent></DetailBoxesNavComponent>
-            <CarDetailBoxesComponent></CarDetailBoxesComponent>
+        <div style={{ flex: "10" }}>
+          <DetailBoxesNavComponent></DetailBoxesNavComponent>
+          <CarDetailBoxesComponent></CarDetailBoxesComponent>
         </div>
-       </DispJustAlgn>
+      </DispJustAlgn>
     </DispJustAlgn>
-    
-  )
-}
+  );
+};
 
-export default MotorComponent
+export default MotorComponent;
